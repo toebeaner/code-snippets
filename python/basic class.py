e8ebbb2c6fb5e362c01000c264b9f969a24eb2bd
toebeaner <119559348+toebeaner@users.noreply.github.com>
2023-02-09 17:11:53 -0800

    Update basic class.py

--- python/basic class.py
+++ python/basic class.py
@@ -1,17 +1,45 @@
+str_1 = "string 1!!!"
+
+
+
 class example:
-  def __init__(self, test):
+  def __init__(self, data_1 = "No data provided", data_2 = "No data provided"):
 
 
-    self.test = test
+    self._1 = data_1
+    self._2 = data_2
 
 
-  def message(self):
-    return self.test
+  def data(self):
+    return f"- {self._1}\n- {self._2}" # line 9/10
+  
+  def view(self):
+    class class_example(object):
+      data_1 = self._1 # line 9
+      data_2 = self._2 # line 10
+    return class_example
 
 
 
 
 
-penis = example("crack")
+main_example = example(str_1) # str / line 1
 
-print(penis.message())
+
+print(main_example.data()) # func / line 13
+#Expected response
+"""
+- string 1!!!
+- No data provided
+"""
+
+
+class_example = main_example.view() # func / line 16
+
+print("+ "+class_example.data_1)
+print("+ "+class_example.data_2)
+# Expected response 
+"""
++ string 1!!!
++ No data provided
+"""
