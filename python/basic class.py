405775c0cdaca6f9b97d745bad4411ab715d53cd
Toebeaner <119559348+toebeaner@users.noreply.github.com>
2023-04-05 15:53:59 -0700

    Update basic class.py

--- python/basic class.py
+++ python/basic class.py
@@ -1,4 +1,4 @@
-str_1 = "string 1!!!"
+
 
 
 
@@ -11,7 +11,7 @@ class example:
 
 
   def data(self):
-    return f"- {self._1}\n- {self._2}" # line 9/10
+    return f"- {self._1}\n{self._2}" # line 9/10
   
   def view(self):
     class class_example(object):
@@ -22,24 +22,27 @@ class example:
 
 
 
+str_1 = "string 1!!!"
 
 main_example = example(str_1) # str / line 1
 
 
+
 print(main_example.data()) # func / line 13
+
 #Expected response
 """
-- string 1!!!
-- No data provided
+string 1!!!
+No data provided
 """
 
 
 class_example = main_example.view() # func / line 16
+print(class_example.data_1)
+print(class_example.data_2)
 
-print("+ "+class_example.data_1)
-print("+ "+class_example.data_2)
 # Expected response 
 """
-+ string 1!!!
-+ No data provided
+string 1!!!
+No data provided
 """
