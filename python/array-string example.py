f734cee3f0dbfd5910958b33e0d3ccac52941e46
Toebeaner <119559348+toebeaner@users.noreply.github.com>
2023-04-05 16:06:53 -0700

    Code Cleanup

--- python/array-string example.py
+++ python/array-string example.py
@@ -1,17 +1,9 @@
+array = [ ["Top Message 1", "Bottom Message 1"], ["Top Message 2", "Bottom Message 2"] ]
 
-data = [ ["Title 1", "Message 1"], ["Title 2", "Message 2"] ]
-
-def func(array = None):
+def func(array = [ "Top Message", "Bottom Message" ]):
     data = ""
-    if array:
-        for x in array:
-            newdata = f"""
-> {x[0]}
-{x[1]}\n""" 
-            data += newdata
+    for string in array:
+        data += f">> {string[0]}\n> {string[1]}\n" 
         return data
-    else:
-        return "error"
-    
 
-print( func(data) )
+print(func(array))
